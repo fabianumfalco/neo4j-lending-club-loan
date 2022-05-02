a637c1867ec9d424eb520de969d036d87a89b133
fabiano.damasceno <fabiano.damasceno@tse.jus.br>
2022-05-02 12:29:58 -0300

    alteração do readme da pasra data

---
+++ data/extract_soft_information.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import argparse
+import os.path
+
+def verifica_arquivo_existe(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("O arquivo  %s não existe!" % arg)
+    else:
+        return open(arg, 'r')  # retorna o handle do arquivo aberto
+
+def main():
+   parser = argparse.ArgumentParser(description='Extrai as informações soft do cliente')
+   parser.add_argument("-i", dest="arqCsvFull", required=True,
+                    help="arquivo com todas as informações do cliente", metavar="ArquivoCSVFull",
+                    type=lambda x: verifica_arquivo_existe(parser, x))
+   parser.add_argument("-o", dest="arqCsvSoft", required=True,
+                    help="arquivo com resultado das informações soft do cliente", metavar="ArquivoCSVSoft")
+   args = parser.parse_args()
+   
+   data = pd.read_csv(args.arqCsvFull)
+   df = pd.DataFrame(data, columns= ['addr_state','annual_inc','emp_length','home_ownership','verification_status','loan_status'])
+   df['borrower_id'] = 'borrower-'+(df.index+1).astype(str)
+   cols = df.columns.tolist()
+   cols = cols[-1:] + cols[:-1]
+   df = df[cols]
+   #print (df)
+   df.to_csv(args.arqCsvSoft, mode='w', sep=',', header=True, index=False, encoding='utf-8-sig')
+   
+
+if __name__ == "__main__":
+   main()
